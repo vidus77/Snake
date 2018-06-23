a325195d9eaffcc2fa48731eee41bb44645984a9
vidus77 <vidus77@gmail.com>
2018-06-23 21:59:29 +0200

    #2: Első lépések - Osztálydefiníció Arena.cs a játék menetének leírására - Példányosítás a MainWindow-ban - Billentyűleütés átvétele az arena.KeyDown függvénnyel az Arena osztályban

--- Snake201806/MainWindow.xaml.cs
+++ Snake201806/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Snake201806.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,28 @@ namespace Snake201806
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private Arena arena;
+
 		public MainWindow()
 		{
+			// 1.	Ctrl . az Arena szón felajánlja betöltésre a "using Snake201806.Model;" névteret,
+			//		mert alapból a C# nem lát bele az alkönyvtárakba, az Arena.CS osztályunk viszont a "Model" 
+			//		mappában van. 
+
+			// 2.	létre kell hozni az arena változót is. (Ctrl . a kisnbetűs arena szón) 
+			//		ezután a MainWindow  { } kpcsai között el kezd működni, azaz a főblakban létrehozunk egy példányt.
 			InitializeComponent();
+			arena = new Arena();
+
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			//	létrehozunk egy függvényt, ami ekapja a billentyűleütéseket
+			//	1.	bepötyögtük az arena.KeyDown(e); 
+			//	2.	Ctrl . a Keydown szón legenrálja az eseményelkapót
+			//	3.	KeyDown fletti F12 - vel ét lehet menni az automatikusan generált függvény definícióhoz az Arena.cs-ben 
+			arena.KeyDown(e);
 		}
 	}
 }
---
+++ Snake201806/Model/Arena.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Snake201806.Model
+{
+	/// <summary>
+	/// A játékmenet logikáját alkalmazza 
+	/// 
+	/// Osztálydefinició  Az osztály egy leírás
+	/// leírja, hogy ha létrehozunk egy példányt, (példányosítjuk), 
+	/// akkor minden létrehozott példány hogyan működjön. 
+	/// 
+	/// Ez olyan, mint egy tervrajz, 
+	/// </summary>
+	class Arena
+	{
+		internal void KeyDown(KeyEventArgs e)
+		{
+			//	milyen eseményeket kezelünk, nem mindet
+			//	csak a nyíl leütéseket,ezért kell nekönk a switch 
+			switch (e.Key)
+			{
+				case Key.Left:
+				//	break;
+				case Key.Up:
+				//	break;
+				case Key.Right:
+				//	break;
+				case Key.Down:
+					Console.WriteLine(e.Key); // cw két Tab
+					break;
+			}
+		}
+	}
+}
